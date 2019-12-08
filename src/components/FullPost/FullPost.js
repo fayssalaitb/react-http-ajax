732827c227764038beac9995dbcb7888e31bcfcc
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-12-08 12:30:00 +0100

    done

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
-        );
-        return post;
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.props.tPost.title}</h1>
+          <p>{this.props.tPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.props.tDelete}>
+              Delete
+            </button>
+          </div>
+        </div>
+      );
     }
+
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      const posts = res.data.slice(0, 4);
+      console.log(posts);
+      const updatedPost = posts.map(post => {
+        return {
+          ...post,
+          author: "Fayssal"
+        };
+      });
+
+      this.setState({ posts: updatedPost });
+    });
+  }
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
+  deleteSelectedData = () => {
+    axios
+      .delete(
+        "https://jsonplaceholder.typicode.com/posts/" +
+          this.state.selectedPostId
+      )
+      .then(res => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+    const postId = this.state.selectedPostId;
+    let fullPost = null;
+
+    if (postId) {
+      const targetedPost = this.state.posts.filter(post => {
+        return post.id === postId;
+      });
+      fullPost = (
+        <FullPost
+          tPost={targetedPost[0]}
+          tDelete={this.deleteSelectedData}
+          id={this.state.selectedPostId}
+        />
+      );
     }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>{fullPost}</section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
